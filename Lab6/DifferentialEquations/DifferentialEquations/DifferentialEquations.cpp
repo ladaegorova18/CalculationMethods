2dcc46968abb4587f85347f02bc7fdaf92a56d2f
ladaegorova18 <st068843@student.spbu.ru>
2020-12-17 00:11:39 +0300

    final version

--- Lab6/DifferentialEquations/DifferentialEquations/DifferentialEquations.cpp
+++ Lab6/DifferentialEquations/DifferentialEquations/DifferentialEquations.cpp
@@ -32,77 +32,84 @@ int main()
     double x0 = 0;
     vector<double> X;
     vector<double> Y;
+    char key = ' ';
 
-    cout << "Введите интервал деления h: ";
-    cin >> h;
-    cout << "\n";
-
-    cout << "Введите число точек правее от х0 (N): ";
-    cin >> N;
-    cout << "\n";
-
-    Solver* solver = new Solver(0, y0, h, N);
-
-    cout << "\x1b[32mМетод Тейлора:\x1b[0m" << endl;
-    for (int k = -2; k <= N; ++k)
+    do
     {
-        double x = x0 + k * h;
-        double preciseSolution = solver->presiseSolution(x);
+        cout << "Введите интервал деления h: ";
+        cin >> h;
+        cout << "\n";
 
-        X.push_back(x);
-        Y.push_back(preciseSolution);
+        cout << "Введите число точек правее от х0 (N): ";
+        cin >> N;
+        cout << "\n";
 
-        double y = solver->Taylor(x);
-        printValue(X[k + 2], y, Y[k + 2], solver);
-        if (k == N) printYn(y, Y[k + 2]);
-    }
-    cout << endl;
+        Solver* solver = new Solver(0, y0, h, N);
 
-    cout << "\x1b[32mМетод Адамса:\x1b[0m" << endl;
+        cout << "\x1b[32mМетод Тейлора:\x1b[0m" << endl;
+        for (int k = -2; k <= N; ++k)
+        {
+            double x = x0 + k * h;
+            double preciseSolution = solver->presiseSolution(x);
 
-    vector<double> yAdams = solver->Adams(N);
-    for (int k = 3; k <= N; ++k)
-    {
-        printValue(X[k + 2], yAdams[k + 2], Y[k + 2], solver);
-        if (k == N) printYn(yAdams[k + 2], Y[k + 2]);
-    }
-    cout << endl;
+            X.push_back(x);
+            Y.push_back(preciseSolution);
 
-    cout << "\x1b[32mМетод Рунге-Кутта:\x1b[0m" << endl;
-    for (int k = 1; k <= N; ++k)
-    {
-        double yRungeKutta = solver->RungeKutta(k);
-        printValue(X[k + 2], yRungeKutta, Y[k + 2], solver);
-        if (k == N) printYn(yRungeKutta, Y[k + 2]);
-    }
-    cout << endl;
+            double y = solver->Taylor(x);
+            printValue(X[k + 2], y, Y[k + 2], solver);
+            if (k == N) printYn(y, Y[k + 2]);
+        }
+        cout << endl;
 
-    cout << "\x1b[32mМетод Эйлера:\x1b[0m" << endl;
-    for (int k = 1; k <= N; ++k)
-    {
-        double yEuler = solver->Euler(X[k + 2], k);
-        printValue(X[k + 2], yEuler, Y[k + 2], solver);
-        if (k == N) printYn(yEuler, Y[k + 2]);
-    }
-    cout << endl;
+        cout << "\x1b[32mМетод Адамса 4-го порядка:\x1b[0m" << endl;
 
-    cout << "\x1b[32mМетод Эйлера I:\x1b[0m" << endl;
-    for (int k = 1; k <= N; ++k)
-    {
-        double yEulerI = solver->EulerI(X[k + 2], k);
-        printValue(X[k + 2], yEulerI, Y[k + 2], solver);
-        if (k == N) printYn(yEulerI, Y[k + 2]);
-    }
-    cout << endl;
+        vector<double> yAdams = solver->Adams(N);
+        for (int k = 3; k <= N; ++k)
+        {
+            printValue(X[k + 2], yAdams[k + 2], Y[k + 2], solver);
+            if (k == N) printYn(yAdams[k + 2], Y[k + 2]);
+        }
+        cout << endl;
 
-    cout << "\x1b[32mМетод Эйлера II:\x1b[0m" << endl;
-    for (int k = 1; k <= N; ++k)
-    {
-        double yEulerII = solver->EulerII(X[k + 2], k);
-        printValue(X[k + 2], yEulerII, Y[k + 2], solver);
-        if (k == N) printYn(yEulerII, Y[k + 2]);
-    }
-    cout << endl;
+        cout << "\x1b[32mМетод Рунге-Кутта:\x1b[0m" << endl;
+        for (int k = 1; k <= N; ++k)
+        {
+            double yRungeKutta = solver->RungeKutta(k);
+            printValue(X[k + 2], yRungeKutta, Y[k + 2], solver);
+            if (k == N) printYn(yRungeKutta, Y[k + 2]);
+        }
+        cout << endl;
+
+        cout << "\x1b[32mМетод Эйлера:\x1b[0m" << endl;
+        for (int k = 1; k <= N; ++k)
+        {
+            double yEuler = solver->Euler(X[k + 2], k);
+            printValue(X[k + 2], yEuler, Y[k + 2], solver);
+            if (k == N) printYn(yEuler, Y[k + 2]);
+        }
+        cout << endl;
+
+        cout << "\x1b[32mМетод Эйлера I:\x1b[0m" << endl;
+        for (int k = 1; k <= N; ++k)
+        {
+            double yEulerI = solver->EulerI(X[k + 2], k);
+            printValue(X[k + 2], yEulerI, Y[k + 2], solver);
+            if (k == N) printYn(yEulerI, Y[k + 2]);
+        }
+        cout << endl;
+
+        cout << "\x1b[32mМетод Эйлера II:\x1b[0m" << endl;
+        for (int k = 1; k <= N; ++k)
+        {
+            double yEulerII = solver->EulerII(X[k + 2], k);
+            printValue(X[k + 2], yEulerII, Y[k + 2], solver);
+            if (k == N) printYn(yEulerII, Y[k + 2]);
+        }
+        cout << endl;
+
+        cout << "Введите q, чтобы выйти:\n";
+        cin >> key;
+    } while (key != 'q');
 }
 
 void printYn(double y, double Yn)
--- Lab6/DifferentialEquations/DifferentialEquations/Solver.h
+++ Lab6/DifferentialEquations/DifferentialEquations/Solver.h
@@ -64,7 +64,6 @@ public:
 		EulerValues.push_back(y0);
 		EulerIValues.push_back(y0);
 		EulerIIValues.push_back(y0);
-
 	}
 
 	double presiseSolution(double x)
@@ -86,7 +85,7 @@ public:
 
 	vector<double> Adams(int n)
 	{
-		vector<double> q, dq, d2q, d3q, d4q, results;
+		vector<double> q, dq, dSqrq, dCubeq, d4q, results;
 		results.push_back(y0);
 
 		for (int i = 0; i < 5; i++) {
@@ -96,23 +95,24 @@ public:
 			dq.push_back(q[i + 1] - q[i]);
 		}
 		for (int i = 0; i < 3; i++) {
-			d2q.push_back(dq[i + 1] - dq[i]);
+			dSqrq.push_back(dq[i + 1] - dq[i]);
 		}
 		for (int i = 0; i < 2; i++) {
-			d3q.push_back(d2q[i + 1] - d2q[i]);
+			dCubeq.push_back(dSqrq[i + 1] - dSqrq[i]);
 		}
 		for (int i = 0; i < 1; i++) {
-			d4q.push_back(d3q[i + 1] - d3q[i]);
+			d4q.push_back(dCubeq[i + 1] - dCubeq[i]);
 		}
+
 		for (int i = 5; i <= n + 2; i++) 
 		{
-			Y[i] = (Y[i - 1] + q[i - 1] + dq[i - 2] / 2 + 5 * d2q[i - 3] / 12 
-				+ 3 * d3q[i - 4] / 8 + 251 * d4q[i - 5] / 720);
+			Y[i] = (Y[i - 1] + q[i - 1] + dq[i - 2] / 2.0 + 5 * dSqrq[i - 3] / 12.0 
+				+ 3 * dCubeq[i - 4] / 8.0 + 251 * d4q[i - 5] / 720.0);
 			q.push_back(h * func->function(X[i], Y[i]));
 			dq.push_back(q[i] - q[i - 1]);
-			d2q.push_back(dq[i - 1] - dq[i - 2]);
-			d3q.push_back(d2q[i - 2] - d2q[i - 3]);
-			d4q.push_back(d3q[i - 3] - d3q[i - 4]);
+			dSqrq.push_back(dq[i - 1] - dq[i - 2]);
+			dCubeq.push_back(dSqrq[i - 2] - dSqrq[i - 3]);
+			d4q.push_back(dCubeq[i - 3] - dCubeq[i - 4]);
 		}
 
 		return Y;
@@ -161,119 +161,3 @@ public:
 		return abs(result - exactValue);
 	}
 };
-
-
-
-//for (int i = n - 4; i <= n; ++i)
-//{
-//	table[0][i] = h * func->equation(X[i], Y[i]);
-//}
-//for (int i = n - 4; i <= n - 1; ++i)
-//{
-//	table[1][i] = table[0][i + 1] - table[0][i];
-//}
-//for (int i = n - 4; i <= n - 2; ++i)
-//{
-//	table[2][i] = table[1][i + 1] - table[1][i];
-//}
-//for (int i = n - 4; i <= n - 3; ++i)
-//{
-//	table[3][i] = table[2][i + 1] - table[2][i];
-//}
-//table[4][n - 4] = table[3][n - 3] - table[3][n - 4];
-
-
-
-/*for (int i = n - 4; i <= n - 1; ++i)
-{
-	table[1][i] = table[0][i + 1] - table[0][i];
-}
-for (int i = n - 4; i <= n - 2; ++i)
-{
-	table[2][i] = table[1][i + 1] - table[1][i];
-}
-for (int i = n - 4; i <= n - 3; ++i)
-{
-	table[3][i] = table[2][i + 1] - table[2][i];
-}
-table[4].push_back(table[3][n - 3] - table[3][n - 4]);*/
-
-//double deltayn = table[0][n] + 0.5 * table[1][n - 1] + (5.0 / 12) * table[2][n - 2] + (3.0 / 8) * table[3][n - 3]
-//	+ (251.0 / 720) * table[4][n - 4];
-//double y = Y[n - 1] + deltayn;
-//Y.push_back(y);
-//return y;
-
-
-
-//double delta = 0;
-//vector<double> result(N + 1);
-//vector<vector<double>> table;
-//
-//for (int i = 0; i < N + 3; ++i)
-//{
-//	vector<double> v(5);
-//	table.push_back(v);
-//}
-//
-//for (int i = 0; i < 5; ++i)
-//{
-//	table[0][i] = h * func->function(X[i], Y[i]);
-//}
-//
-//for (int j = 4; j > 0; j--)
-//{
-//	for (int k = 0; k < j; k++)
-//	{
-//		table[k][5 - j] = table[k + 1][4 - j] - table[k][4 - j];
-//	}
-//}
-//for (int k = 3; k < N + 1; k++)
-//{
-//	delta = table[k + 1][0] + (table[k][1]) / 2.0 + (table[k - 1][2] * 5) / 12.0 + (table[k - 2][3] * 3) / 8.0 + (table[k - 3][4] * 251) / 720.0;
-//
-//	result[k] = result[k - 1] + delta;
-//
-//	table[k + 2][0] = h * func->function(X[k], result[k]);
-//	table[k + 1][1] = table[k + 2][0] - table[k + 1][0];
-//	table[k][2] = table[k + 1][1] - table[k][1];
-//	table[k - 1][3] = table[k][2] - table[k - 1][2];
-//	table[k - 2][4] = table[k - 1][3] - table[k - 2][3];
-//}
-//return result;
-
-
-/*for (int i = n - 4; i <= n - 1; ++i)
-{
-	table[1][i] = table[0][i + 1] - table[0][i];
-}
-for (int i = n - 4; i <= n - 2; ++i)
-{
-	table[2][i] = table[1][i + 1] - table[1][i];
-}
-for (int i = n - 4; i <= n - 3; ++i)
-{
-	table[3][i] = table[2][i + 1] - table[2][i];
-}
-table[4][n - 4] = table[3][n - 3] - table[3][n - 4];*/
-
-//* n += 1;
-//
-//for (int i = n - 4; i <= n; ++i)
-//{
-//	table[0][i] = h * func->function(X[i], Y[i]);
-//}
-//
-//for (int j = 4; j > 0; j--)
-//{
-//	for (int k = n - 4; k < n - 4 + j; k++)
-//	{
-//		table[5 - j][k] = table[4 - j][k + 1] - table[4 - j][k];
-//	}
-//}
-//
-//double deltayn = table[0][n] + 0.5 * table[1][n - 1] + (5.0 / 12) * table[2][n - 2] + (3.0 / 8) * table[3][n - 3]
-//+ (251.0 / 720) * table[4][n - 4];
-//double y = Y[n] + deltayn;
-//Y.push_back(y);
-//return y; */
