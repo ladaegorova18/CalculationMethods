cec174fe306d7fa58a58c7dd560e495ff7d62c7a
ladaegorova18 <st068843@student.spbu.ru>
2021-05-14 04:15:29 +0300

    работающая версия

--- Semester6/Task2/main.py
+++ Semester6/Task2/main.py
@@ -12,9 +12,13 @@ myB = [ [-0.527466],
  [2.526877],
  [5.165441]]
 
+
+def toFixed(numObj, digits=0):
+    return f"{numObj:.{digits}f}"
+
 # --- end of исходные данные
 
-
+# 1)
 # --- вывод системы на экран
 def FancyPrint(A, B, selected):
     for row in range(len(B)):
@@ -62,99 +66,108 @@ def Gauss(A, B):
     X = [0 for b in B]
     for i in range(len(B) - 1, -1, -1):
         X[i] = B[i][0] - sum(x * a for x, a in zip(X[(i + 1):], A[i][(i + 1):]))
-    print("\n".join("X{0} =\t{1:10.2f}".format(i + 1, x) for i, x in
-enumerate(X)))
     return X
 # --- end of решение системы методом Гаусса (приведением к треугольному виду)
+
+
+def printVector(X):
+    print("\n".join("X{0} =\t{1:10.2f}".format(i + 1, x) for i, x in enumerate(X)))
+
+# 1)
 print("Исходная система:")
 FancyPrint(myA, myB, None)
-print("Решаем:")
+print("Решаем методом Гаусса:")
+xGauss = Gauss(np.copy(myA), np.copy(myB))
+printVector(xGauss)
+print("-------------------------------------------------")
 
-x = Gauss(myA, myB)
-
+# 2)
 def norminf (H):
-    maxNorm = 0.0
-    for i in range (0, 3):
-        sum = 0.0;
-        for j in range (0, 3):
-            sum += abs(myA[i][j])
-        maxNorm = max(maxNorm, sum)
-    return maxNorm
+    return np.amax(H)
 
+# 3)
+def infprecision(k, H, g):
+    f = norminf(H)
+    return (f ** k) * norminf(np.zeros((3, 1))) + f ** k * norminf(g) / (1 - f)
+
+# 4)
 def frobnormvector(X):
     sum = 0.0
     for i in range(len(X)):
         c = X[i]
-        sum += abs(X[i])
+        sum += (abs(X[i])) ** 2
     return sum ** 0.5
+
 
 def frobnorm (A):
     sum = 0.0
     for i in range(len(A)):
         for j in range(len(A[0])):
-            sum += abs(A[i][j])
+            sum += (abs(A[i][j])) ** 2
     return sum ** 0.5
+
 
 def aprprecision (k, H, g):
     f = frobnorm(H)
-    return (f ** k) * frobnorm(np.zeros((3, 1))) + f ** k * frobnorm(g) / (1 - f)
+    return (f ** k) * frobnormvector(np.zeros((3, 1))) + f ** k * frobnormvector(g) / (1 - f)
+
 
 def apostprecision (H, xk, xkplus1):
     return frobnorm(H) * frobnorm(xk - xkplus1) / (1 - frobnorm(H))
 
+
 def lusthernik (H, xk, xkplus1):
     x_ = xk + (xkplus1 - xk) / (1 - max(abs(np.linalg.eigvals(H))))
-    return xkplus1 + x_
+    return x_
+
 
 def makezero():
     return np.zeros((3,1))
 
-def subtract_vectors(v, w):
-    return [vi - wi for vi, wi in zip(v, w)]
 
-def factprecision(a):
-    b = x
+def factprecision(x):
+    b = np.copy(xGauss)
     for i in range(3):
-        b[i] = b[i] - a[i]
+        k = b[i] - x[i][0]
+        b[i] = k
     prec = frobnormvector(b)
+    #prec = np.linalg.norm(b)
     print("Фактическая погрешность: ", prec)
     return prec
 
-def spectradius(H):
-    return np.max(np.abs(np.linalg.eigvals(H)))
-
-def toFixed(numObj, digits=0):
-    return f"{numObj:.{digits}f}"
 
 def simpleiter (k, H, g):
     xk = makezero()
+    xklust = makezero()
     for i in range (k):
         xkplus1 = H @ xk + g
-        xk = xkplus1
-    print ("Решение: ", xkplus1)
-    factprecision(xkplus1)
+        xklust = np.copy(xk)
+        xk = np.copy(xkplus1)
+    print ("Решение: ")
+    print(xkplus1)
+    fiter = factprecision(xkplus1)
     print ("Априорная оценка: ", aprprecision(k, H, g))
     print("Апостериорная оценка: ", apostprecision(H, xk, xkplus1))
-    l = lusthernik(H, xk, xkplus1)
-    print("Решение, уточнённое по Люстернику: ", l)
+    l = lusthernik(H, xklust, xkplus1)
+    print("Решение, уточнённое по Люстернику: ")
+    print(l)
     factprecision(l)
-    return xkplus1
+    return xkplus1, fiter
 
+
+# 5)
 def zeidel (k, H, g):
     xk = makezero()
-    xkplus1 = xk
-    for p in range (k):
-        for i in range (3):
-            s = 0
-            s1 = 0
-            for j in range (i):
-                s = H[i][j] * xkplus1[j][0]
-            for j in range (i, 3):
-                s1 = H[i][j] * xk[j]
-            xkplus1[i] = s + s1 + g[i]
-        xk = xkplus1
-    factprecision(xkplus1)
+    Hl = np.tril(H, k=-1)
+    Hr = np.triu(H, k=0)
+    E = np.eye(3, 3, dtype=np.double)
+    Hseid = np.linalg.inv(E - Hl) @ Hr
+    gSeid = np.linalg.inv(E - Hl) @ g
+    for i in range (k):
+        xkplus1 = Hseid @ xk + gSeid
+        xk = np.copy(xkplus1)
     return xkplus1
+
 
 def radius (H):
     Hl = np.tril(H, k=-1)
@@ -163,6 +176,12 @@ def radius (H):
     x = np.linalg.inv(E - Hl) @ Hr
     return spectradius(x)
 
+
+def spectradius(H):
+    return np.max(np.abs(np.linalg.eigvals(H)))
+
+
+# 7)
 def upperrelax(k, H, g):
     p = spectradius(H)
     q = 2 / (1 + np.sqrt(1 - p * p))
@@ -175,41 +194,55 @@ def upperrelax(k, H, g):
             for j in range(i + 1, 3):
                 sum1 += H[i][j] * xk[j]
             xkplus1[i] = xk[i] + q * (sum + sum1 - xk[i] + g[i])
-    print("Метод верхней релаксации: ", xkplus1)
-    factprecision(xkplus1)
     return xkplus1
 
-# 1)
-x = Gauss(myA, myB)
 
 # 2)
 E = np.eye(3, 3, dtype=np.double)
 Hd = E - inv(np.diag(np.diag(myA))) @ myA
 gD = inv(np.diag(np.diag(myA))) @ myB
 print("Норма H: ", norminf(Hd))
+print("-------------------------------------------------")
+
 
 # 3)
-print("Априорная погрешность для х(7): ", aprprecision(7, Hd, gD))
+print("Априорная погрешность для х(7): ", infprecision(7, Hd, gD))
+print("-------------------------------------------------")
+
 
 # 4)
-xsimp = simpleiter(7, Hd, gD)
-print("Метод простой итерации: ", xsimp)
+print("Метод простой итерации: ")
+xsimp, fiter = simpleiter(7, Hd, gD)
+print("-------------------------------------------------")
+
 
 # 5)
+print("Метод Зейделя: ")
 xseid = zeidel(7, Hd, gD)
-print("Метод Зейделя: ", xseid)
+print(xseid)
+fseid = factprecision(xseid)
 
+print("Сравним с решением, полученным методом простой итерации:")
 print(xsimp - xseid)
+print("-------------------------------------------------")
+
 
 # 6)
 print("Радиус: ", radius(Hd))
+print("-------------------------------------------------")
+
 
 # 7)
+print("Метод верхней релаксации: ")
 uppx = upperrelax(7, Hd, gD)
+print(uppx)
+fuppx = factprecision(uppx)
+print("-------------------------------------------------")
+
 
 # 8)
 Cond = []
-Cond.append([factprecision(x), factprecision(xsimp), factprecision(xseid), factprecision(uppx)])
+Cond.append([0.0, fiter, fseid, fuppx])
 
 print(tabulate(Cond, headers=['Gauss', 'Simple iteration', 'Seidel', 'Successive over-relaxation'],
                tablefmt='pipe', numalign="right"))
